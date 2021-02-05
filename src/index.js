2ba516ea05d4fdbec8e4f34d773531139ce86cff
周智勤 <kreviazukzhou@gmail.com>
2021-02-05 17:44:44 +0800

    1

--- src/index.js
+++ src/index.js
@@ -1,2 +1,11 @@
-require('./index.css');
-require('./index.less');
+import _ from 'lodash'
+function component() {
+  const element = document.createElement('div');
+
+  // lodash（目前通过一个 script 引入）对于执行这一行是必需的
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+  return element;
+}
+
+document.body.appendChild(component());
--- webpack.md
+++
@@ -1,2 +0,0 @@
-## 1 -D 只开发依赖,上线不需要 
-## 2 清除每次打包前清除dist目录 clean-webpack-plugin
